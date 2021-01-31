2d8b85d02f11d6da54bb6296fe7e29533bce916e
menduoy <menduoy@gmail.com>
2021-01-31 17:33:33 +0900

    Regenerate user[s] controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get '/signup', to: 'user#new'
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/help',  to: 'static_pages#help'
+  resources :users
 end
