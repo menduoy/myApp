0c72f632178ed9615cb40f1f3227e4826d374be2
menduoy <menduoy@gmail.com>
2021-02-02 12:01:38 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get '/signup', to: 'user#new'
+  get '/signup', to: 'users#new'
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/help',  to: 'static_pages#help'
